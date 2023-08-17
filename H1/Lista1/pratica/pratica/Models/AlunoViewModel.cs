7c1aee11d1c5c62f3c10e3989a716a930536bd03
ThieresKB <thieresmda@gmail.com>
2023-08-16 21:19:20 -0300

    Começando a aprender DDD

---
+++ H1/Lista1/pratica/pratica/Models/AlunoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace pratica.Model
+{
+    public class AlunoViewModel
+    {
+        public string nome { get; set; }
+        public string ra { get; set; }
+        public string email { get; set; }
+        public string cpf { get; set; }
+        public int ativo { get; set; }
+    }
+}
